ad0f404eece325733f917d2578656088bff751d6
Dener Silva <dener3d.designer@gmail.com>
2024-02-07 15:43:10 -0300

    feat: pdf join

---
+++ src/controllers/controllers.py
@@ -0,0 +1,21 @@
+from typing import Dict
+import io
+from io import BytesIO
+import PyPDF2
+import uuid
+
+class Controller:
+    def join_pdf(self, pdf_bytes) -> Dict:
+        pdf_merger = PyPDF2.PdfMerger()
+        
+        for pdf in pdf_bytes:
+            pdf_file = PyPDF2.PdfReader(io.BytesIO(pdf))
+            pdf_merger.append(pdf_file)
+            
+        uid = str(uuid.uuid1())
+        path = 'merged_files/' + uid + '.pdf'
+        
+        with open(path, 'wb') as output_file:
+            pdf_merger.write(output_file)
+        
+        return {"path": path}
---
+++ test.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from barcode import Code128
+from barcode.writer import ImageWriter
+from src.controllers.controllers import Controller
+
+app = Flask(__name__)
+
+@app.route('/join_pdf', methods={'POST'})
+def join_pdf():
+    controller = Controller()
+    files = request.files.getlist('pdfs')
+    print(files)
+     
+    bytes_pdf = []
+    for pdf in files:
+        bytes_pdf.append(pdf.read())
+    
+    response = controller.join_pdf(bytes_pdf)
+    
+    return {"res": response}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=3000, debug=True)
