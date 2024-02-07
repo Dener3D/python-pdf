111695aaddf177026a5ee84abc14e40aeb353e0b
Dener Dev <dener3d.designer@gmail.com>
2024-02-07 16:26:47 -0300

    Update test.py

--- test.py
+++ test.py
@@ -21,5 +21,5 @@ def join_pdf():
     return {"res": response}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
