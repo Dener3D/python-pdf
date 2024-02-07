7cafd3e7a25a3420d0e495369c874bf6f61a244c
Dener Dev <dener3d.designer@gmail.com>
2024-02-07 16:47:48 -0300

    Update controllers.py

--- src/controllers/controllers.py
+++ src/controllers/controllers.py
@@ -16,7 +16,7 @@ class Controller:
             pdf_merger.append(pdf_file)
             
         uid = str(uuid.uuid1())
-        path = os.path.join('https://pacific-earth-60757-12860c7b633c.herokuapp.com/', uid + '.pdf')
+        path = os.path.join('http://172.18.133.166:14781/', uid + '.pdf')
         
         with open(path, 'wb') as output_file:
             pdf_merger.write(output_file)
