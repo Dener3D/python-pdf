809df5b9c19011a1b6f33e30471a77fa6a0aefed
Dener Silva <dener3d.designer@gmail.com>
2024-02-08 13:38:32 -0300

    feat: split pdf

--- src/controllers/controllers.py
+++ src/controllers/controllers.py
@@ -4,8 +4,8 @@ from io import BytesIO
 import PyPDF2
 import uuid
 import os
-
-url = 'https://pacific-earth-60757-12860c7b633c.herokuapp.com/'
+from PyPDF2 import PdfReader, PdfWriter
+from flask import jsonify
 
 class Controller:
     def join_pdf(self, pdf_bytes) -> Dict:
@@ -21,3 +21,34 @@ class Controller:
             pdf_merger.write(output_file)
         
         return {"path": path}
+    
+    def split_pdf(self, pdf, root_path) -> Dict:
+        output_files = []
+        try:
+            # Obtém o arquivo PDF em bytes do corpo da requisição
+            pdf_bytes = pdf.read()
+
+            # Cria um leitor de PDF a partir dos bytes recebidos
+            pdf_reader = PdfReader(io.BytesIO(pdf_bytes))
+
+            # Diretório onde os PDFs divididos serão salvos
+            output_directory = root_path  # Pode ser ajustado conforme necessário
+            
+            uid = str(uuid.uuid1())
+
+            # Itera sobre cada página do PDF e salva como um novo arquivo
+            for page_number in range(len(pdf_reader.pages)):
+                # Cria um escritor de PDF para a página atual
+                pdf_writer = PdfWriter()
+                pdf_writer.add_page(pdf_reader.pages[page_number])
+
+                # Salva o PDF dividido localmente
+                output_filepath = os.path.join(output_directory, f'{uid}_page_{page_number + 1}.pdf')
+                output_files.append(f'{uid}_page_{page_number + 1}.pdf')
+                with open(output_filepath, 'wb') as output_file:
+                    pdf_writer.write(output_file)
+
+            return jsonify({'status': 'success', 'message': 'PDF dividido e salvo com sucesso', "files": output_files})
+
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)})
--- test.py
+++ test.py
@@ -1,15 +1,46 @@
 from flask import Flask, request, jsonify, send_file
 from src.controllers.controllers import Controller
 import os
+import time
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+def delete_pdfs():
+    try:
+        # Diretório raiz do projeto
+        root_directory = app.root_path
+
+        # Obtém a lista de arquivos na raiz do projeto
+        files_in_root = os.listdir(root_directory)
+
+        # Filtra apenas os arquivos PDF
+        pdf_files = [file for file in files_in_root if file.endswith('.pdf')]
+
+        # Exclui cada arquivo PDF encontrado
+        for pdf_file in pdf_files:
+            pdf_filepath = os.path.join(root_directory, pdf_file)
+            os.remove(pdf_filepath)
+
+        return {'status': 'success', 'message': 'Arquivos PDF excluídos com sucesso'}
+
+    except Exception as e:
+        return {'status': 'error', 'message': str(e)}
+    
+def delete_pdfs_job():
+    with app.app_context():
+        delete_pdfs()
+
+scheduler = BackgroundScheduler()
+
+scheduler.add_job(func=delete_pdfs_job, trigger='interval', minutes=60)
+scheduler.start()
+
+controller = Controller()
+
 @app.route('/join_pdf', methods=['POST'])
-def join_pdf():
-    controller = Controller()
+def join_pdf(): 
     files = request.files.getlist('pdfs')
-    print(files)
-     
     bytes_pdf = []
     for pdf in files:
         bytes_pdf.append(pdf.read())
@@ -17,6 +48,11 @@ def join_pdf():
     response = controller.join_pdf(bytes_pdf)
     
     return {"res": response}
+
+@app.route('/split_pdf', methods=['POST'])
+def split_pdf():
+    pdf = request.files.get('pdf')
+    return controller.split_pdf(pdf, app.root_path)
 
 @app.route('/download_pdf', methods=['GET'])
 def download_pdf():
@@ -27,13 +63,14 @@ def download_pdf():
         print(filepath)
         # Verifica se o arquivo existe
         if os.path.exists(filepath):
-            # Envia o arquivo como resposta à solicitação GET
-            return send_file(filepath, as_attachment=True)
+            #with open(filepath, 'rb') as file:
+            response = send_file(filepath, as_attachment=True, mimetype='application/pdf', download_name='merged_file.pdf')
+            # Remove o arquivo após o download
+            return response
         else:
             return {'status': 'error', 'message': 'Arquivo não encontrado'}
 
     except Exception as e:
-        print("HSAHDASHDAS")
         return {'status': 'error', 'message': str(e)}
 
 if __name__ == "__main__":
